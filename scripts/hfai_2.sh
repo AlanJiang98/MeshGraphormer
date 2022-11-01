b9b91173fec362fbef8fb30a79a518194cbeaadd
Bingxuan Wang <wangbx@pku.edu.cn>
2022-11-01 21:20:56 +0800

    fixed render error

--- scripts/hfai_2.sh
+++ scripts/hfai_2.sh
@@ -1,6 +1,6 @@
 python -m torch.distributed.launch --nproc_per_node=8 scripts/train_hfai.py \
-    --config src/configs/hfai/fastmetro_rgb_aug_mask.yaml \
-    --output_dir output/fastmetro_rgb_aug_mask
+    --config src/configs/hfai/check_fast_rgb_aug.yaml \
+    --output_dir output/check_fast_rgb_aug
 
 
 # CUDA_VISIBLE_DEVICES=7 python scripts/train.py \
